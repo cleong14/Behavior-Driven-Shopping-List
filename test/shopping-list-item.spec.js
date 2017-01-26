6f04f08ed758f88a17cb473f85f0d5ae5e1b669e
Chaz Leong <cleong14@gmail.com>
2017-01-25 21:38:11 -1000

    added 3 tests to shopping-list-item.spec.js and added comments to shopping-list.spec.js

--- test/shopping-list-item.spec.js
+++ test/shopping-list-item.spec.js
@@ -10,7 +10,17 @@ describe('ShoppingListItem', function () {
   // ShoppingListItem is a class
   // a class is a 'special function'
   // keyword = function
-  it('should be a function', function () {
-    expect(ShoppingListItem).to.be.a('function');
+  it("should be a function", function () {
+    expect(ShoppingListItem).to.be.a("function");
+  });
+
+  // ShoppingListItem has property named 'name'
+  it("should have a property named 'name'", function () {
+    expect(ShoppingListItem).to.have.property("name");
+  });
+
+  // ShoppingListItem has a propety name description
+  it("should have a property named 'description'", function () {
+    expect(ShoppingListItem).to.have.property("description");
   });
 });
--- test/shopping-list.spec.js
+++ test/shopping-list.spec.js
@@ -5,6 +5,8 @@ var should = chai.should();
 
 describe('ShoppingList', function () {
 
+  // 'it' refers to what youre describing
+  // in this case, it refers to 'ShoppingList'
   it('should be a function', function () {
     expect(ShoppingList).to.be.a('function');
   });
